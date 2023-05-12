283749d13882ab71247fc972f6bec466973f26e4
Karuna Tata <74637789+starlightknown@users.noreply.github.com>
2023-05-12 15:48:02 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 import Chatbox from './Chatbox';
+import logo from './logo.png'; // Update the path if necessary
+import { FaEnvelope, FaSlack, FaRobot, FaHandshake, FaTicketAlt } from 'react-icons/fa';
 
 const App: React.FC = () => {
   const [chatInput, setChatInput] = useState('');
@@ -13,36 +15,87 @@ const App: React.FC = () => {
     setChatInput(message);
   };
 
-  const handleGetStarted = () => {
-    handleChatboxSubmit('Hi');
+  const handleConversations = () => {
+    handleChatboxSubmit('Connect Slack/Email');
+  };
+
+  const handleTickets = () => {
+    handleChatboxSubmit('Set up tickets');
+  };
+
+  const handleDeflection = () => {
+    handleChatboxSubmit('Deflection and Auto responses');
+  };
+
+  const handleService = () => {
+    handleChatboxSubmit('How to upload articles?');
   };
 
   return (
     <div className="page">
       <div className="container">
         <div className="box">
-          <div className="card">
-            <h1 className="title">Designed For Work</h1>
-            <p className="subtitle">
-              Introducing the first ever 5G enabled tablet. You've got a tablet that lets you
-              play harder and work smarter.
-            </p>
-            <button className="btn" onClick={handleGetStarted}>
-              Get Started
-            </button>
-          </div>
-          <div className="card">{/* Add additional cards here */}</div>
-          <div className="card">{/* Add additional cards here */}</div>
+        <div className="card">
+  <div className="card-logo">
+  <FaSlack size={30} /> <FaEnvelope size={30}/>
+  </div>
+  <h1 className="title">Customer Conversations</h1>
+  <p className="subtitle">
+    Bring all your customer conversations at one place. Connect email or Slack. Click on Connect Conversations to know more.
+  </p>
+  <button className="btn" onClick={handleConversations}>
+    Connect Conversations
+  </button>
+</div>
+<div className="card">
+  <div className="card-logo">
+  <FaTicketAlt size={30}/>
+  </div>
+  <h1 className="title">Convos to Tickets</h1>
+  <p className="subtitle">
+    Sometimes conversations are not everything, trace your conversations and turn them into tickets by using Slack and PLuG widget.
+  </p>
+  <button className="btn" onClick={handleTickets}>
+    Set up tickets
+  </button>
+</div>
+<div className="card">
+  <div className="card-logo">
+    <FaRobot size={30}/>
+  </div>
+  <h1 className="title">Auto Responses and AI for Deflection</h1>
+  <p className="subtitle">
+    Answer common questions by GPT-powered customer responses. Set up auto response based on your team availability.
+  </p>
+  <button className="btn" onClick={handleDeflection}>
+    Configure Responses and Deflection
+  </button>
+</div>
+<div className="card">
+  <div className="card-logo">
+    <FaHandshake size={30}/>
+  </div>
+  <h1 className="title">Self Service</h1>
+  <p className="subtitle">
+    Got articles on your product which can give answers? Upload them and allow users to search through documentation via PLuG.
+  </p>
+  <button className="btn" onClick={handleService}>
+    Upload Articles
+  </button>
+</div>
+
           {/* Add more cards as needed */}
         </div>
         <div className="chatbox-container">
+        <div className="logo-container">
+  <img src={logo} alt="Logo" className="logo-image" />
+</div>
           <Chatbox
             chatInput={chatInput}
             onChatboxSubmit={handleChatboxSubmit}
             onChatInputChange={handleChatInputChange}
           />
         </div>
-        <div className="blob"></div>
       </div>
     </div>
   );
--- src/Chatbox.tsx
+++ src/Chatbox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 interface ChatboxProps {
   chatInput: string;
@@ -7,18 +7,29 @@ interface ChatboxProps {
 }
 
 const Chatbox: React.FC<ChatboxProps> = ({ chatInput, onChatInputChange, onChatboxSubmit }) => {
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<{ content: string; isUser: boolean }[]>([]);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (chatInput.trim() !== '') {
-      setMessages((prevMessages) => [...prevMessages, chatInput]);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { content: chatInput, isUser: true },
+      ]);
       onChatboxSubmit(chatInput);
       // Clear the input field
       onChatInputChange({ target: { value: '' } } as React.ChangeEvent<HTMLInputElement>);
 
       const botResponse = generateBotResponse(chatInput);
-      setMessages((prevMessages) => [...prevMessages, botResponse]);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { content: botResponse, isUser: false },
+      ]);
     }
   };
 
@@ -36,6 +47,18 @@ const Chatbox: React.FC<ChatboxProps> = ({ chatInput, onChatInputChange, onChatb
       case 'what is your name?':
         response = 'My name is Chatbot.';
         break;
+      case 'connect slack/email':
+        response = '&lt;a href="https://devrev.ai/marketplace/devrev-plug-with-email"&gt;Email integration&lt;/a&gt; is a predefined automation that has been used by several organizations. It allows you to manage support conversations that come through email directly from the DevRev PLuG inbox, eliminating the need to manage two inboxes. This integration allows you to manage all end-user communication from one place so you never have to leave the context of the conversation to create a ticket and communicate with your development team. Email integration through SendGrid and other providers with DMARC are also available. <br><br> Connect with &lt;a href="https://devrev.ai/marketplace/slack""&gt;Slack&lt;/a&gt and ensure your customer or internal conversations are always in sync.';
+        break;
+      case 'set up tickets':
+          response = 'You can create tickets in DevRev from Slack messages. To activate this feature in a private channel, you will need to invite the @DevRev bot. Use the ⋮ actions menu to create a ticket from an existing Slack conversation. When you use this action, your ticket will contain a link back to the Slack conversations. <br><br> Install the PLuG widget into your application with just a few lines of code and immediately bring the voice of your customer to your entire team. You can also test the PLuG widget by clicking DevRev Org settings (top left DevRev icon) -> Support -> Try out PLuG. Follow step by step guide and copy and paste code from the link here -> &lt;a href="https://devrev.ai/docs/plug/installation"&gt;Installation&lt;/a&gt';
+          break;
+      case 'deflection and auto responses':
+         response = '&lt;a href="https://devrev.ai/marketplace/article-suggestion""&gt;Deflection&lt;/a&gt - Instead of sending a generic message with a link to one or more articles that may match the customer query, respond with the specific paragraph from that article that is most likely to answer their question. The first step is to upload and publish the knowledge base articles that you would like to be made available to your customers through DevRev. This is done in Settings > Articles. Next, enable this snap-in by clicking the Install button and following the installation steps. <br><br> &lt;a href="https://devrev.ai/marketplace/auto-reply""&gt;Auto response&lt;/a&gt - You can take control of what DevRev bot replies to the customer when your customer sends the first message to your PLuG inbox. You can also set different messages during working hours and non-working hours. Include a call to action to your users within your auto replies with customizable, interactive buttons. Use these to prompt actions such as book a demo, watch video, and read documentation.';
+          break;
+      case 'how to upload articles?':
+          response = 'Login to your org and go to "Settings". Under the support category, you will find "Articles". Click the button and upload articles or add links. You can also copy paste this link to do it - `https://app.devrev.ai/org-slug/settings/articles`. Replace `org-slug` with your org slug.';
+            break;
       default:
         response = "I'm sorry, I don't understand.";
         break;
@@ -44,24 +67,51 @@ const Chatbox: React.FC<ChatboxProps> = ({ chatInput, onChatInputChange, onChatb
     return response;
   };
 
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  };
+
+  const renderMessageContent = (content: string) => {
+    const urlRegex = /(&lt;a href="[^"]*"&gt;[^<]*<\/a&gt;)/g;
+    const sanitizedContent = content.replace(urlRegex, (match) => {
+      const linkRegex = /&lt;a href="([^"]*)"&gt;([^<]*)<\/a&gt;/;
+      const [, url, text] = match.match(linkRegex) || [];
+      if (url && text) {
+        return `<a href="${url}&gt;${text}</a>`;
+      }
+      return match;
+    });
+    const decodedContent = new DOMParser().parseFromString(sanitizedContent, 'text/html').documentElement.textContent;
+    return { __html: decodedContent } as { __html: string | TrustedHTML };
+  };
+
   return (
     <div className="chatbox">
       <div className="chatbox-messages">
         {messages.map((message, index) => (
-          <div key={index} className="chatbox-message">
-            {message}
+          <div
+            key={index}
+            className={`chatbox-message ${message.isUser ? 'user-message' : 'bot-message'}`}
+          >
+            <div
+              className="message-content"
+              dangerouslySetInnerHTML={renderMessageContent(message.content)}
+            ></div>
           </div>
         ))}
+        <div ref={messagesEndRef}></div>
       </div>
-      <form className="chatbox-input" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Type your message"
-          value={chatInput}
-          onChange={onChatInputChange}
-        />
-        <button type="submit">Send</button>
-      </form>
+      <div className="chatbox-input-container">
+        <form className="chatbox-input" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Type your message"
+            value={chatInput}
+            onChange={onChatInputChange}
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   );
 };
